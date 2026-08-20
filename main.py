6b523829b1ac5fe4d9f9164e6fd9d5871b76fa3c
mt0262318-hub <mt0262318@gmail.com>
2026-08-20 08:40:26 +0000

    Auto-Sync: 2026-08-20 08:40:26

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-cat << 'EOF' > main.py
 import os
 import sys
 import io
@@ -930,5 +929,3 @@ if __name__ == "__main__":
     else:
         import uvicorn
         uvicorn.run("main:app", host="0.0.0.0", port=8501, reload=True)
-EOF
-docker restart rian_fastapi
